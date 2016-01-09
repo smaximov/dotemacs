86aed398dcfc3a512c17f39c045c3a249bfb335b
smaximov <s.b.maximov@gmail.com>
2016-01-10 02:45:59 +1000

    Fix Emacs not knowing about `org-directory`
    `org-directory` should be used only after org-mode is loaded

--- init.el
+++ init.el
@@ -92,5 +92,6 @@
 (setq flycheck-checkers '(javascript-eslint))
 
 ;; Org Mode
-(setq org-default-notes-file (concat org-directory "/notes.org"))
+(eval-after-load 'org
+  '(setq org-default-notes-file (concat org-directory "/notes.org")))
 (define-key global-map "\C-cc" 'org-capture)
