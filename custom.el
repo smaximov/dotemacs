601e72c68aef4d887fcba66814b1da572e15e500
smaximov <s.b.maximov@gmail.com>
2016-02-01 12:13:58 +1000

    Bookmark+

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(bmkp-last-as-first-bookmark-file "/home/nameless/.emacs.d/bookmarks")
  '(flycheck-haskell-runhaskell "~/bin/stack-runhaskell"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
