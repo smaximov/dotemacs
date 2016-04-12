dce397cbd39f06dff450cece4957a135daabb45d
smaximov <s.b.maximov@gmail.com>
2016-04-12 19:18:44 +1000

    magit: use annotated tags by default

--- custom.el
+++ custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(bmkp-last-as-first-bookmark-file "/home/nameless/.emacs.d/bookmarks")
- '(flycheck-haskell-runhaskell "~/bin/stack-runhaskell"))
+ '(flycheck-haskell-runhaskell "~/bin/stack-runhaskell")
+ '(magit-tag-arguments (quote ("--annotate"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
