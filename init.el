2f632b92c1f03f11d2afde2ceebd8d93130d7dd9
smaximov <s.b.maximov@gmail.com>
2016-04-21 23:52:34 +1000

    multiple-cursors: add the package

--- init.el
+++ init.el
@@ -379,6 +379,15 @@ frame is created."
   :config
   (setf projectile-switch-project-action 'neotree-projectile-action))
 
+(use-package multiple-cursors
+  :ensure t
+  :bind
+  (("C-S-c C-S-c" . mc/edit-lines)
+   ("C->" . mc/mark-next-like-this)
+   ("C-<" . mc/mark-previous-like-this)
+   ("C-c C->" . mc/mark-all-like-this)
+   ("C-S-<mouse-1>" . mc/add-cursor-on-click)))
+
 ;;; Custom commands
 
 ;;; Other configuration
