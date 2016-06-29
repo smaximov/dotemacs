02d4e32342d50a7009432127ac330c70f44a4ae4
smaximov <s.b.maximov@gmail.com>
2016-06-29 20:18:50 +1000

    custom.el: auto-update selected packages list

--- custom.el
+++ custom.el
@@ -5,7 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(bmkp-last-as-first-bookmark-file "/home/nameless/.emacs.d/bookmarks")
  '(flycheck-haskell-runhaskell "~/bin/stack-runhaskell")
- '(magit-tag-arguments nil))
+ '(magit-tag-arguments nil)
+ '(package-selected-packages
+   (quote
+    (lua-mode yasnippet yaml-mode toml-mode tldr simple-httpd scss-mode rvm req-package racer php-mode phi-search org neotree mustache-mode mustache multiple-cursors material-theme markdown-mode magit json-mode js2-mode inf-ruby htmlize helm-projectile helm-descbinds git flycheck-rust flycheck-haskell exec-path-from-shell emr elixir-mode editorconfig dockerfile-mode darkroom company-tern cider cask-mode bookmark+))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
