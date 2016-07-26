14318972c1e1fb9bb73f88a51c69656420039311
smaximov <s.b.maximov@gmail.com>
2016-07-26 19:21:13 +1000

    init: load autoinsert

---
+++ init.d/init-autoinsert.el
@@ -0,0 +1,12 @@
+;;; init-autoinsert.el --- configure and load `autoinsert'  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'req-package)
+
+(req-package autoinsert :loader :built-in)
+
+(provide 'init-autoinsert)
+;;; init-autoinsert.el ends here
