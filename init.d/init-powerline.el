2c5ae16e8db40d76314b4bdbb0a2d26bf03325db
smaximov <s.b.maximov@gmail.com>
2016-08-08 01:13:07 +1000

    init: install powerline

---
+++ init.d/init-powerline.el
@@ -0,0 +1,14 @@
+;;; init-powerline.el --- powerline setup -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'req-package)
+
+(req-package powerline
+  :config
+  (powerline-default-theme))
+
+(provide 'init-powerline)
+;;; init-powerline.el ends here
