17cf201fdf94a3e13dc57018f73d05d741f5f8e9
smaximov <s.b.maximov@gmail.com>
2016-08-10 01:45:21 +1000

    cask: add hook to update load path of Cask-managed projects

--- init.d/init-cask.el
+++ init.d/init-cask.el
@@ -11,5 +11,29 @@
   :init
   (add-hook 'cask-mode-hook #'enable-paredit-mode))
 
+(req-package cask
+  :require dash f
+  :config
+  (defun cask-update-load-path ()
+    (interactive)
+    (-when-let* ((project-path (and buffer-file-name
+                                    (locate-dominating-file buffer-file-name cask-filename)))
+                 (bundle (cask-setup project-path))
+                 (elisp-files (--map (f-canonical (f-expand it project-path))
+                                     (--filter (equal "el" (f-ext it))
+                                               (cask-files bundle)))))
+      (when (-contains? elisp-files buffer-file-name)
+        (let* ((elisp-dirs (-distinct (--map (f-dirname it) elisp-files)))
+               (deps-dirs (-non-nil (--map (cask-dependency-path bundle
+                                                                 (cask-dependency-name it))
+                                           (cask-runtime-dependencies bundle t))))
+               (extra-paths (append elisp-dirs deps-dirs))
+               (new-paths (append extra-paths (default-value 'load-path))))
+          (set (make-local-variable 'load-path) new-paths)
+          (when (boundp 'flycheck-emacs-lisp-load-path)
+            (set (make-local-variable 'flycheck-emacs-lisp-load-path)
+                 new-paths))))))
+  (add-hook 'emacs-lisp-mode-hook #'cask-update-load-path))
+
 (provide 'init-cask)
 ;;; init-cask.el ends here
