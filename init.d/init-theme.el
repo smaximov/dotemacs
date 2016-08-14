692282a76220910f385722763c41e30adf57d8e0
smaximov <s.b.maximov@gmail.com>
2016-08-15 02:29:32 +1000

    theme: fix theme loading in daemon mode

--- init.d/init-theme.el
+++ init.d/init-theme.el
@@ -6,10 +6,27 @@
 
 (require 'req-package)
 
+(defvar nameless:theme-gui-loaded nil)
+(defvar nameless:theme-terminal-loaded nil)
+
+(defun nameless:load-theme-once (theme)
+  "Ensure THEME is loaded only once."
+  (if (window-system)
+      (unless nameless:theme-gui-loaded
+        (if nameless:theme-terminal-loaded
+            (enable-theme theme)
+          (load-theme theme t))
+        (setf nameless:theme-gui-loaded t))
+    (unless nameless:theme-terminal-loaded
+      (if nameless:theme-gui-loaded
+          (enable-theme theme)
+        (load-theme theme t))
+      (setf nameless:theme-terminal-loaded t))))
+
 (req-package material-theme
   :config
   (with-daemon
-   (load-theme 'material t)))
+   (nameless:load-theme-once 'material)))
 
 (provide 'init-theme)
 ;;; init-theme.el ends here
