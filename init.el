2078439512d15f1041ede32550b1ff1a1049930a
smaximov <s.b.maximov@gmail.com>
2016-08-24 12:31:45 +1000

    auto-insert: elisp package skeleton

--- init.el
+++ init.el
@@ -112,6 +112,16 @@ in BODY."
 ;; Diminish
 (diminish 'auto-revert-mode)
 
+;; Emacs package header skeleton
+(define-auto-insert '(emacs-lisp-mode . "Emacs Package Skeleton")
+  '("Short description: "
+    ";;; " (file-name-nondirectory buffer-file-name) " --- " str " -*- lexical-binding: t -*-" ?\n ?\n
+    ";;; Commentary:" ?\n ?\n
+    ";;; Code:" ?\n ?\n
+    _ ?\n ?\n
+    "(provide '" (file-name-base) ")" ?\n
+    ";;; " (file-name-nondirectory buffer-file-name) " ends here"))
+
 ;; Start Emacs maximized
 (with-daemon
  (when (window-system)
