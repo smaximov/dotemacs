c1d222f066e964ae15800a1f81abfa6768b23b86
smaximov <s.b.maximov@gmail.com>
2016-08-30 14:13:21 +1000

    emmet-mode: some customizations

--- init.d/init-emmet.el
+++ init.d/init-emmet.el
@@ -9,7 +9,10 @@
 (req-package emmet-mode
   :init
   (add-hook 'sgml-mode-hook #'emmet-mode)
-  (add-hook 'css-mode-hook #'emmet-mode))
+  (add-hook 'css-mode-hook #'emmet-mode)
+  :config
+  (setf emmet-move-cursor-between-quotes t
+        emmet-self-closing-tag-style " /"))
 
 (provide 'init-emmet)
 ;;; init-emmet.el ends here
