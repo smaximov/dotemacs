e81a09046eaeb0fcd036f4c310f0cf42a3a4ab55
smaximov <s.b.maximov@gmail.com>
2016-09-11 17:55:42 +1000

    elm-mode: enable completion and update README

--- init.d/init-elm.el
+++ init.d/init-elm.el
@@ -6,7 +6,11 @@
 
 (require 'req-package)
 
-(req-package elm-mode)
+(req-package elm-mode
+  :require company
+  :config
+  (add-hook 'elm-mode-hook #'elm-oracle-setup-completion)
+  (add-to-list 'company-backends #'company-elm))
 
 (provide 'init-elm)
 ;;; init-elm.el ends here
