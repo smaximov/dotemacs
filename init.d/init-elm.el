3fa51bb3b376995494e7eecd10c6101b5efde6d4
smaximov <s.b.maximov@gmail.com>
2016-09-11 17:47:11 +1000

    init: install elm-mode

---
+++ init.d/init-elm.el
@@ -0,0 +1,12 @@
+;;; init-elm.el --- a function language for the Web -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'req-package)
+
+(req-package elm-mode)
+
+(provide 'init-elm)
+;;; init-elm.el ends here
