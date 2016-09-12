bff44914fea6bdd6894347b48643a5b292639fb5
smaximov <s.b.maximov@gmail.com>
2016-09-12 14:24:12 +1000

    yasnippet(elm-mode): add Elm snippets

--- init.d/init-elm.el
+++ init.d/init-elm.el
@@ -7,7 +7,40 @@
 (require 'req-package)
 
 (req-package elm-mode
-  :require company
+  :require company f dash json
+  :preface
+  (defconst nameless:elm-package "elm-package.json")
+
+  (defun nameless:parse-elm-package-src (package-file)
+    "Parse `src' array of PACKAGE-FILE."
+    (with-temp-buffer
+      (insert-file-contents package-file)
+      (goto-char (point-min))
+      (let ((json-object-type 'hash-table)
+            (json-array-type 'list))
+        (gethash "source-directories" (json-read)))))
+
+  (defun nameless:resolve-relative-path (file directories)
+    "Resolve FILE relative to DIRECTORIES."
+    (catch 'done
+      (--each directories
+        (when (f-ancestor-of? it file)
+          (throw 'done (f-relative file it))))))
+
+  (defun nameless:elm-buffer-module-name ()
+    "Return the current Elm buffer's corresponding module name."
+    (when buffer-file-name
+      (-if-let* ((project-root (locate-dominating-file buffer-file-name
+                                                       nameless:elm-package))
+                 (package-file (f-join project-root nameless:elm-package))
+                 (source-dirs (--map (f-join project-root it)
+                                     (nameless:parse-elm-package-src package-file)))
+                 (relative-path (nameless:resolve-relative-path buffer-file-name
+                                                                source-dirs)))
+          (replace-regexp-in-string (regexp-quote (f-path-separator)) "."
+                                    (f-no-ext relative-path)
+                                    'case-sensitive 'literal)
+        (f-base buffer-file-name))))
   :config
   (add-hook 'elm-mode-hook #'elm-oracle-setup-completion)
   (add-hook 'elm-mode-hook #'electric-pair-local-mode)
