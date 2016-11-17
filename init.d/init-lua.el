e908d5b2d7179020b43e7038de51cc092e019c7b
smaximov <s.b.maximov@gmail.com>
2016-11-17 19:43:52 +0300

    init: lua-mode

---
+++ init.d/init-lua.el
@@ -0,0 +1,12 @@
+;;; init-lua.el --- major mode for editing Lua -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'req-package)
+
+(req-package lua-mode)
+
+(provide 'init-lua)
+;;; init-lua.el ends here
