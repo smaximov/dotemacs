413a2f984aae480531a3c32ac8a1d02bad7e4d2b
smaximov <s.b.maximov@gmail.com>
2016-11-18 12:44:37 +0300

    init: don't save Emacs PID on startup

--- init.el
+++ init.el
@@ -6,28 +6,6 @@
 
 ;;; Code:
 
-;; Save PID file when running as a daemon
-(require 'server)
-
-(when (daemonp)
-  (defvar emacs-pid-dir (or (getenv "XDG_RUNTIME_DIR")
-                            server-socket-dir
-                            temporary-file-directory))
-  (defvar emacs-pid-file (format "%s/%s" emacs-pid-dir "emacs.pid"))
-
-  ;; write PID file on startup
-  (add-hook 'emacs-startup-hook
-            (lambda ()
-              (make-directory emacs-pid-dir t)
-              (with-temp-file emacs-pid-file
-                (insert (number-to-string (emacs-pid))))))
-
-  ;; remove PID file on exit
-  (add-hook 'kill-emacs-hook
-            (lambda ()
-              (when (file-exists-p emacs-pid-file)
-                (delete-file emacs-pid-file)))))
-
 ;; Initialize Emacs package system
 (require 'package)
 (setf package-enable-at-startup nil
