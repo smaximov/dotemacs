550972fbc09dcb6f71743c13ab3dda8eca1f47eb
smaximov <s.maximov@teachmeplease.com>
2016-12-05 19:06:03 +0300

    init: install yaml-path

--- init.d/init-yaml.el
+++ init.d/init-yaml.el
@@ -9,5 +9,11 @@
 (req-package yaml-mode
   :mode "\\.yml$")
 
+(req-package yaml-path :loader :path :force t
+  :load-path "lib"
+  :require yaml-mode
+  :bind (:map yaml-mode-map
+              ("C-c C-p" . yaml-path/path)))
+
 (provide 'init-yaml)
 ;;; init-yaml.el ends here
