79060982e5e1bb180cca9a830aea5a6384237a11
smaximov <s.b.maximov@gmail.com>
2017-02-18 19:11:17 +0300

    init: install julia-mode, julia-shell

---
+++ init.d/init-julia.el
@@ -0,0 +1,20 @@
+;;; init-julia.el --- julia language -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'req-package)
+
+(req-package julia-mode)
+
+(req-package julia-shell
+  :require julia-mode
+  :mode ("\\.jl$" . julia-mode)
+  :bind (:map julia-mode-map
+              ("C-x C-e" . julia-shell-run-region-or-line)
+              ("C-c C-l" . julia-shell-save-and-go)
+              ("C-c C-s" . run-julia)))
+
+(provide 'init-julia)
+;;; init-julia.el ends here
