1d5c9ac632132c7d47f7f66040d05deb9ad3562e
smaximov <s.b.maximov@gmail.com>
2017-05-11 12:28:37 +0300

    magit: add a command to retreive current jira issue number from branch

--- init.d/init-magit.el
+++ init.d/init-magit.el
@@ -7,9 +7,21 @@
 (require 'req-package)
 
 (req-package magit
+  :preface
+  (defun nameless:magit-insert-current-jira-issue-number ()
+    "Get current current JIRA issue number from branch name and insert it at point."
+    (interactive)
+    (let ((branch (magit-get-current-branch)))
+      (if branch
+          (save-match-data
+            (if (string-match "TCHM[-_]\\([[:digit:]]+\\)" branch)
+                (insert (format "TCHM-%s " (match-string-no-properties 1 branch)))
+              (user-error "This branch doesn't reference a JIRA issue")))
+        (user-error "There is no current branch"))))
   :require validate
   :bind (("C-x g" . magit-status)
-         ("C-x C-g" . magit-dispatch-popup))
+         ("C-x C-g" . magit-dispatch-popup)
+         ("C-x j" . nameless:magit-insert-current-jira-issue-number))
   :if (version<= "24.4" emacs-version)
   :init
   (add-hook 'after-init-hook #'global-magit-file-mode)
