72722048a0d2d9a71b1bfdd0204fe5ebf7903511
smaximov <s.b.maximov@gmail.com>
2017-11-09 19:04:05 +0300

    magit: allow specifying JIRA issue prefix per project

--- init.d/init-magit.el
+++ init.d/init-magit.el
@@ -8,19 +8,37 @@
 
 (req-package magit
   :preface
+  (defconst nameless:magit-dotjira ".jira")
+
   (defun nameless:magit-insert-current-jira-issue-number ()
     "Get current current JIRA issue number from branch name and insert it at point."
     (interactive)
-    (let ((branch (magit-get-current-branch)))
-      (if branch
-          (save-match-data
-            (if (string-match "^\\([A-Z]+\\)[-_]\\([[:digit:]]+\\)" branch)
-                (insert (format "%s-%s "
-                                (match-string-no-properties 1 branch)
-                                (match-string-no-properties 2 branch)))
-              (user-error "This branch doesn't reference a JIRA issue")))
-        (user-error "There is no current branch"))))
-  :require validate
+    (if current-prefix-arg
+        (nameless:magit-insert-current-jira-issue-prefix)
+      (let ((branch (magit-get-current-branch)))
+        (if branch
+            (save-match-data
+              (if (string-match "^\\([A-Z]+\\)[-_]\\([[:digit:]]+\\)" branch)
+                  (insert (format "%s-%s "
+                                  (match-string-no-properties 1 branch)
+                                  (match-string-no-properties 2 branch)))
+                (nameless:magit-insert-current-jira-issue-prefix)))
+          (user-error "This branch doesn't reference a JIRA issue")))))
+
+  (defun nameless:magit-insert-current-jira-issue-prefix ()
+    (interactive)
+    (-when-let* ((root (locate-dominating-file default-directory
+                                               nameless:magit-dotjira))
+                 (dotjira (concat root nameless:magit-dotjira)))
+      (if (file-exists-p dotjira)
+          (insert (format "%s-"
+                          (string-trim-right
+                           (with-temp-buffer
+                             (insert-file-contents dotjira)
+                             (buffer-string)))))
+        (user-error "Unable to infer JIRA issue number"))))
+
+  :require validate dash f
   :bind (("C-x g" . magit-status)
          ("C-x C-g" . magit-dispatch-popup)
          ("C-x j" . nameless:magit-insert-current-jira-issue-number))
