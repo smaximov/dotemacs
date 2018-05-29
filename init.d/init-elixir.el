dd7460aedd615417d2e76ca506251c3d10a81655
smaximov <s.b.maximov@gmail.com>
2018-05-29 17:29:24 +0300

    init(elixir): enable elixir format on buffer save

--- init.d/init-elixir.el
+++ init.d/init-elixir.el
@@ -8,7 +8,22 @@
 
 (req-package elixir-mode
   :ensure t
-  :pin melpa-stable)
+  :pin melpa
+  :require exec-path-from-shell projectile validate
+  :preface
+  (defun set-default-directory-to-mix-project-root (original-fun &rest args)
+    (if-let* ((mix-project-root (and (projectile-project-p)
+                                     (projectile-locate-dominating-file buffer-file-name
+                                                                        ".formatter.exs"))))
+        (let ((default-directory mix-project-root))
+          (apply original-fun args))
+      (apply original-fun args)))
+  :hook ((elixir-mode . (lambda () (add-hook 'before-save-hook #'elixir-format nil t))))
+  :init
+  (advice-add 'elixir-format :around #'set-default-directory-to-mix-project-root)
+  :config
+  (validate-setq elixir-format-elixir-path (executable-find "elixir")
+                 elixir-format-mix-path (executable-find "mix")))
 
 (req-package alchemist
   :ensure t
