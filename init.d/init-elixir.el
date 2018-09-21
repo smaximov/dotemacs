7dbde2f74b173faf1530b0d83091ff45f2cfcf96
smaximov <s.b.maximov@gmail.com>
2018-09-21 10:49:12 +0300

    init(elixir-mode): setup formatter args, remove obsolete variables

--- init.d/init-elixir.el
+++ init.d/init-elixir.el
@@ -13,21 +13,28 @@
 (req-package elixir-mode
   :ensure t
   :pin melpa
-  :require exec-path-from-shell projectile validate
+  :require projectile
   :preface
   (defun set-default-directory-to-mix-project-root (original-fun &rest args)
-    (if-let* ((mix-project-root (and (projectile-project-p)
-                                     (projectile-locate-dominating-file buffer-file-name
-                                                                        ".formatter.exs"))))
+    (if-let ((mix-project-root (and (projectile-project-p)
+                                    (projectile-locate-dominating-file buffer-file-name
+                                                                       "mix.exs"))))
         (let ((default-directory mix-project-root))
           (apply original-fun args))
       (apply original-fun args)))
-  :hook ((elixir-mode . (lambda () (add-hook 'before-save-hook #'elixir-format nil t))))
+
+  (defun setup-elixir-format-arguments ()
+    (setq-local elixir-format-arguments
+                (when-let ((formatter-dir (and (projectile-project-p)
+                                               (projectile-locate-dominating-file
+                                                buffer-file-name
+                                                ".formatter.exs"))))
+                  `("--dot-formatter" ,(concat formatter-dir ".formatter.exs")))))
+  :hook ((elixir-mode . (lambda ()
+                          (add-hook 'before-save-hook #'elixir-format nil t)))
+         (elixir-format . setup-elixir-format-arguments))
   :init
-  (advice-add 'elixir-format :around #'set-default-directory-to-mix-project-root)
-  :config
-  (validate-setq elixir-format-elixir-path (executable-find "elixir")
-                 elixir-format-mix-path (executable-find "mix")))
+  (advice-add 'elixir-format :around #'set-default-directory-to-mix-project-root))
 
 (req-package alchemist
   :ensure t
