091633ae34c8fef57435bd6ab42c785afbff4285
Sergei Maximov <s.b.maximov@gmail.com>
2019-01-30 10:41:14 +0300

    flycheck: don't wrap flycheck commands for Ruby in bundle exec

--- init.d/init-flycheck.el
+++ init.d/init-flycheck.el
@@ -11,15 +11,7 @@
   :require diminish direnv
   :diminish flycheck-mode
   :pin melpa-stable
-  :hook (after-init . global-flycheck-mode)
-  :custom
-  (flycheck-command-wrapper-function
-   (lambda (command)
-     (let ((bundler (executable-find "bundle"))
-           (cmd (file-name-nondirectory (car command))))
-       (if (string-equal cmd "rubocop")
-           (append (list bundler "exec" "rubocop") (cdr command))
-         command)))))
+  :hook (after-init . global-flycheck-mode))
 
 (provide 'init-flycheck)
 ;;; init-flycheck.el ends here
