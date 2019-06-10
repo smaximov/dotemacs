dcd4c7aa36f67862ef98a72dded80795b7a22ff0
Sergei Maximov <s.b.maximov@gmail.com>
2019-06-10 14:54:33 +0300

    alchemist: set erlang/elixir sources to aid the goto-definition feature

--- init.d/init-elixir.el
+++ init.d/init-elixir.el
@@ -39,7 +39,16 @@
 (req-package alchemist
   :ensure t
   :pin melpa-stable
-  :require elixir-mode)
+  :require elixir-mode direnv
+  :preface
+  (defun set-erlang/elixir-source (&rest args)
+    (when (eq major-mode 'elixir-mode)
+        (when-let* ((erlang-src (getenv "ERLANG_SRC")))
+          (setq-local alchemist-goto-erlang-source-dir erlang-src))
+        (when-let* ((elixir-src (getenv "ELIXIR_SRC")))
+          (setq-local alchemist-goto-elixir-source-dir elixir-src))))
+  :init
+  (advice-add 'direnv-update-directory-environment :after #'set-erlang/elixir-source))
 
 (req-package flycheck-credo
   :ensure t
