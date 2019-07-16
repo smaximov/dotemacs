3f461fef3d12180bd52eb42efcfacc6545457141
Sergei Maximov <s.b.maximov@gmail.com>
2019-07-16 17:54:29 +0300

    typescript: resolve local tslint executable in node_modules

--- init.d/init-typescript.el
+++ init.d/init-typescript.el
@@ -12,12 +12,21 @@
 
 (req-package tide
   :ensure t
-  :require typescript-mode
+  :require typescript-mode flycheck
   :preface
   (defun nameless:setup-tide-mode ()
     (tide-setup)
     (tide-hl-identifier-mode)
-    (add-hook 'before-save-hook #'tide-format-before-save))
+    (add-hook 'before-save-hook #'tide-format-before-save)
+    (when-let* ((tslint (nameless:locate-tslint)))
+      (setq-local flycheck-typescript-tslint-executable tslint)))
+
+  (defun nameless:locate-tslint ()
+    (when-let* ((root (locate-dominating-file
+                       (or (buffer-file-name) default-directory)
+                       "node_modules"))
+                (tslint (expand-file-name "node_modules/.bin/tslint" root)))
+      (and (file-executable-p tslint) tslint)))
   :hook (typescript-mode . nameless:setup-tide-mode))
 
 (provide 'init-typescript)
