b0a46761a8b90b79beaf21698cd40889d4c2259c
Sergei Maximov <s.b.maximov@gmail.com>
2020-04-13 20:07:14 +0300

    crystal-mode: add keybindings for running tests

--- init.d/init-crystal.el
+++ init.d/init-crystal.el
@@ -10,7 +10,12 @@
   :ensure t
   :pin melpa
   :hook ((crystal-mode . (lambda ()
-                           (add-hook 'before-save-hook #'crystal-tool-format nil t)))))
+                           (add-hook 'before-save-hook #'crystal-tool-format nil t))))
+  :bind (:map crystal-mode-map
+              ("C-c , a" . crystal-spec-all)
+              ("C-c , v" . crystal-spec-buffer)
+              ("C-c , s" . crystal-spec-line)
+              ("C-c , t" . crystal-spec-switch)))
 
 (req-package flycheck-crystal
   :ensure t
