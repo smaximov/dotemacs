be2a8849b3b1010e029221997a2cf80fbd37dcf0
Sergei Maximov <s.b.maximov@gmail.com>
2020-10-03 11:51:33 +0300

    init: add unison-mode

---
+++ init.d/init-unison.el
@@ -0,0 +1,13 @@
+;;; init-unison.el --- unison lang -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'req-package)
+
+(req-package unisonlang-mode
+  :ensure t)
+
+(provide 'init-unison)
+;;; init-unison.el ends here
