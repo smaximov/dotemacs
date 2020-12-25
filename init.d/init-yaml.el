158b94645111b64e4778922d4826a2b69c4f81b1
Sergei Maximov <s.b.maximov@gmail.com>
2020-12-25 15:14:54 +0300

    yaml: drop yaml-path

--- init.d/init-yaml.el
+++ init.d/init-yaml.el
@@ -13,12 +13,5 @@
   :require highlight-indentation
   :hook (yaml-mode . highlight-indentation-current-column-mode))
 
-(req-package yaml-path
-  :disabled
-  :load-path "lib"
-  :require yaml-mode
-  :bind (:map yaml-mode-map
-              ("C-c C-p" . yaml-path/path)))
-
 (provide 'init-yaml)
 ;;; init-yaml.el ends here
